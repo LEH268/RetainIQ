9a8464522dfb976501c4c2e6937083f5b45a3c76
LEH268 <earnhuilee@gmail.com>
2026-07-19 00:07:31 +0800

    update vercel.json

--- retainiq-backend/main.py
+++ retainiq-backend/main.py
@@ -17,7 +17,7 @@ app = FastAPI(title="RetainIQ API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://retain-iq-self.vercel.app/"],
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
